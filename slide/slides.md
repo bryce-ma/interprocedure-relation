74a93e6922f969b5758e3f01e12f5b4ef10448f6
bryce-ma <lmfy0717@gmail.com>
2018-12-26 16:15:12 +0800

    add slide example

---
+++ slide/slides.md
@@ -0,0 +1,24 @@
+---
+theme: uncover
+class:
+footer: "南京大学 · 软件分析与测试"
+---
+
+# Python过程间调用分析
+
+##### 马洪跃
+
+##### 2018-12-29
+
+##### Github项目地址 [interprocedure-relation](https://github.com/bryce-ma/interprocedure-relation)
+
+---
+
+<!-- paginate: true -->
+
+# PyCallGraph
+
+![pycallgraph](./asset/pycallgraph.png)
+
+---
+
--- test/input.py
+++ test/input.py
@@ -1,20 +1,24 @@
-class A:
-    def method0(self, name):
-        print(name)
-        if 2 > 1:
-            print('2 > 1')
-            self.m1()
+class Apple:
+    def __init__(self, name):
+        self.name = name
+    def color(self):
+        lname = self.name.lower()
+        if lname == 'gala' or lname == 'fuji':
+            self.red(self.name)
+            return 'red'
         else:
-            print('something wrong')
-            self.m2()
-        self.m2()
+            self.green(self.name)
+            return 'green'
 
-    def m1(self):
-        pass
-    def m2(self):
-        pass
+    def red(self, rname):
+        print('Are u want to eat red apples? so '+ rname + ' is red. Plz')
+    def green(self, gname):
+         print('Are u want to eat green apples? so '+ gname + ' is red. Plz')
+
 def main():
-    a = A()
-    a.method0('hello')
+    apple = Apple('fuji')
+    color = apple.color()
+    print('The color of this apple is: '+ color)
+
 if __name__ == '__main__':
     main()
